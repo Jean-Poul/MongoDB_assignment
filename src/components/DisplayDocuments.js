a3c1f5d422ed33305b08a7d4f88d5bd59a569ae8
Jean-Poul <cph-jl360@cphbusiness.dk>
2022-03-10 13:38:50 +0100

    landingpage + display components

---
+++ src/components/DisplayDocuments.js
@@ -0,0 +1,2 @@
+import React, { useState, useEffect } from 'react';
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
---
+++ src/components/Landingpage.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+
+
+const Landingpage = () => {
+
+    return (
+        <div className="font-link">
+            {/* <img src={insert picture} fluid /> */}
+            <div className="pageContent">
+            <div className="textMove">
+                <h1>
+                    Welcome to our twitter diplay
+                </h1>
+                <h4>
+                   NO SOUP FOR YOU!
+                </h4>
+                <div>
+                    <p>Use the navigation at the top of the site</p>
+                </div>
+            </div>
+        </div>
+        </div>
+
+
+    )
+
+}
+
+export default Landingpage;
